9cb440cecfed955d40232778b4d453cc1e2d60eb
sanjay-murmu-au9 <sanjay-murmu-au9@users.noreply.github.com>
2026-04-02 00:28:36 +0000

    deploy: a254e04be87010032635423a8eea1d5596706263

---
+++ assets/WallpaperPlugin-DSFtr-95.js
@@ -0,0 +1,1 @@
+import{r,_ as l}from"./index-CxRmFBsH.js";const i=r("WallpaperPlugin",{web:()=>l(()=>import("./WallpaperPluginWeb-Bnvwic6j.js"),[],import.meta.url).then(e=>new e.WallpaperPluginWeb)});export{i as WallpaperPlugin};
