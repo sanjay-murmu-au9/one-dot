531dee90703ae80e00ad3fd54b2462b0d6e1c4bc
sanjay-murmu-au9 <sanjay-murmu-au9@users.noreply.github.com>
2026-04-04 19:42:19 +0000

    deploy: 54a58e96a29c647ca8e0ba81d3456090d9067130

---
+++ assets/WallpaperPlugin-CHkvNHSN.js
@@ -0,0 +1,1 @@
+import{r,_ as l}from"./index-CdA0SH8n.js";const i=r("WallpaperPlugin",{web:()=>l(()=>import("./WallpaperPluginWeb-78tCdkgN.js"),[]).then(e=>new e.WallpaperPluginWeb)});export{i as WallpaperPlugin};
