2d3a0207908845ecb355cb8447c5aefd5e8c7d51
sanjay-murmu-au9 <sanjay-murmu-au9@users.noreply.github.com>
2026-04-07 17:55:37 +0000

    deploy: 667f346dbe754df97ec6c57860f798ec59ccb500

--- assets/WallpaperPlugin-DGtGWoSS.js
+++
@@ -1,1 +0,0 @@
-import{r,_ as l}from"./index-OemO-ahJ.js";const i=r("WallpaperPlugin",{web:()=>l(()=>import("./WallpaperPluginWeb-78tCdkgN.js"),[]).then(e=>new e.WallpaperPluginWeb)});export{i as WallpaperPlugin};
